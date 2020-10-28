b73353cd8320e0c69ffae0e2506b8d65c526decf
David Rio Deiros <driodeiros@gmail.com>
2020-10-28 17:15:30 -0400

    iteration

--- cmd/golights.go
+++ cmd/golights.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/drio/golights"
@@ -12,16 +13,16 @@ func main() {
 	theStrip := golights.Strip{Size: numLeds, Port: 7777, Ip: "192.168.8.150"}
 	err := theStrip.Connect()
 	if err != nil {
-		fmt.Println("error: ", err)
+		log.Fatal("error: ", err)
 	}
 	defer theStrip.Conn.Close()
 
+	oneSecond, _ := time.ParseDuration("1s")
 	err = theStrip.AllOn(golights.Color{R: 0, G: 0, B: 255})
 	if err != nil {
-		fmt.Println("error: ", err)
+		log.Fatal("error: ", err)
 	}
 
-	oneSecond, _ := time.ParseDuration("500ms")
 	time.Sleep(oneSecond)
 	err = theStrip.AllOn(golights.Color{R: 0, G: 255, B: 0})
 	if err != nil {
@@ -35,13 +36,25 @@ func main() {
 	}
 
 	time.Sleep(oneSecond)
-	err = theStrip.TurnOn(golights.Pixel{Idx: 45, RGB: golights.Color{R: 255}})
+	err = theStrip.TurnOn(golights.Pixel{Idx: 40, RGB: golights.Color{R: 255}})
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
 
 	time.Sleep(oneSecond)
-	err = theStrip.TurnOn(golights.Pixel{Idx: 45, RGB: golights.Color{R: 0}})
+	err = theStrip.TurnOn(golights.Pixel{Idx: 40, RGB: golights.Color{R: 0}})
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	time.Sleep(oneSecond)
+	err = theStrip.TurnOn(golights.Pixel{Idx: 63, RGB: golights.Color{R: 255}})
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	time.Sleep(oneSecond)
+	err = theStrip.TurnOn(golights.Pixel{Idx: 63, RGB: golights.Color{R: 0}})
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
@@ -65,17 +78,25 @@ func main() {
 
 	time.Sleep(oneSecond)
 	theStrip.AllOn(golights.Color{R: 0, G: 0, B: 0})
+	time.Sleep(oneSecond)
+
+	for i := 28; i < 75; i++ {
+		theStrip.TurnOn(golights.Pixel{Idx: uint32(i), RGB: golights.Color{R: 55, G: 55, B: 55}})
+		time.Sleep(aBit)
+		fmt.Println(i)
+	}
 
 	oneMil, _ := time.ParseDuration("50ms")
-	for i := 0; i < 255; i++ {
+	for i := 0; i < 255; i = i + 5 {
 		theStrip.AllOn(golights.Color{R: uint32(i), G: uint32(i), B: uint32(i)})
 		time.Sleep(oneMil)
 	}
-	for i := 255; i >= 0; i-- {
+	for i := 255; i >= 0; i = i - 5 {
 		theStrip.AllOn(golights.Color{R: uint32(i), G: uint32(i), B: uint32(i)})
 		time.Sleep(oneMil)
 		fmt.Println(i)
 	}
 
+	time.Sleep(oneSecond)
 	theStrip.AllOn(golights.Color{R: 0, G: 0, B: 0})
 }
